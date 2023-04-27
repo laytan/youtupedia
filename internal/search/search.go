a45e91dca21728aca279ecf855eb89a6dd464e9a
Laytan Laats <laytanlaats@hotmail.com>
2023-04-27 03:24:54 +0200

    perf: optimize stemming

--- internal/search/search.go
+++ internal/search/search.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"sort"
 	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/laytan/youtupedia/internal/stem"
@@ -31,7 +32,8 @@ func Channel(ctx context.Context, ch *store.Channel, query string) (res []Result
 	// Retrieves the videos that contain all the words we query.
 	// These are optimistic matches, because they have to be in order,
 	// and they can span the metadata boundaries, and we have to return the exact part of the transcripts.
-	videos, err := Queries.VideosOfChannelWithWords(ctx, ch.ID, stem.StemLineWords(query))
+	stemmedQuery := stem.StemLine(query)
+	videos, err := Queries.VideosOfChannelWithWords(ctx, ch.ID, strings.Split(stemmedQuery, " "))
 	if err != nil {
 		return nil, fmt.Errorf("retrieving channel videos: %w", err)
 	}
@@ -43,7 +45,7 @@ func Channel(ctx context.Context, ch *store.Channel, query string) (res []Result
 	for _, vid := range videos {
 		vid := vid
 		group.Go(func() error {
-			results, err := Video(&vid, query)
+			results, err := Video(&vid, stemmedQuery)
 			if err != nil {
 				return fmt.Errorf("searching: %w", err)
 			}
@@ -106,8 +108,8 @@ func Channel(ctx context.Context, ch *store.Channel, query string) (res []Result
 //
 // Optimized to be fast, this is done in O(n) time where n is the length of the searchable_transcript.
 //
-// The query and the transcript is stemmed using the stem package, so different "styles" of the same word
-// will match.
+// The transcript is stemmed using the stem package, so different "styles" of the same word
+// will match. NOTE: you must stem the input query yourself.
 //
 // If the match is on the boundary of a transcript (so part is on transcript/line 1 and other part on 2),
 // The second transcript's ID is returned.
@@ -116,7 +118,7 @@ func Video(vid *store.Video, query string) (res []int64, err error) {
 	var matching int
 	var idStart int
 	var idEnd int
-	runes := []rune(stem.StemLine(query))
+	runes := []rune(query)
 	for i, ch := range vid.SearchableTranscript {
 		if matching == len(runes) {
 			id, err := strconv.ParseInt(vid.SearchableTranscript[idStart:idEnd], 10, 64)
